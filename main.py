955cbf70f56d359a1e7af8847faaaac335854e6a
igdovis <igdovis@gmail.com>
2023-08-06 17:31:35 +0200

    changes to main

--- main.py
+++ main.py
@@ -6,19 +6,28 @@ from butterflydata import ButterflyDataset
 import os
 import numpy as np
 import pandas as pd
+import torchvision.transforms as transforms
+from model import CNN as CNN
+norm_transform = transforms.Compose(
+    [transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 train_dataset = ButterflyDataset(
     labels="data/Training_set.csv",
     img_dir="data/test/",
-    transform=None,
+    transform=norm_transform,
     target_transform=None
 )
 test_dataset = ButterflyDataset(
     labels="data/Testing_set.csv",
     img_dir="data/test/",
-    transform=None,
+    transform=norm_transform,
     target_transform=None
 )
-train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True)
-x, y = next(iter(train_dataset))
-print(x)
+train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+x, y = next(iter(train_dataloader))
+print(x.shape)
+cnn = CNN()
+x = x.reshape(-1)
+print(x.shape)
+output = cnn(x)
+print(output.shape)
